e64e355f123f082c8195c4e67d0d38ac5d62d9a2
LOBSTER10000 <ghh112034@gmail.com>
2024-11-14 22:32:30 +0900

    backend charge solved

--- compressiony_back/src/fileconvert/fileconvert.controller.ts
+++ compressiony_back/src/fileconvert/fileconvert.controller.ts
@@ -2,11 +2,8 @@ import { Controller, Get, Post, Body, Patch, Param, Delete, Req, Res, UseInterce
 import { FileconvertService } from './fileconvert.service';
 import { FilesInterceptor } from '@nestjs/platform-express';
 import { fileUploadMulterOptions } from './fileconvert.multer';
-<<<<<<< HEAD
-=======
 import * as fs from 'fs';
 import { CreateFilesDto } from 'src/dto/create-files.dto';
->>>>>>> 323b0f0b5748f96e8cb104951a116e243c2db3c2
 
 @Controller('fileconvert')
 export class FileconvertController {
@@ -17,17 +14,11 @@ export class FileconvertController {
   
   @Post('/uploadFile')
   @UseInterceptors(FilesInterceptor('file', null, fileUploadMulterOptions))
-<<<<<<< HEAD
-  async uploadFile(@Req() request, @Res() response, @UploadedFiles() files : Express.Multer.File[], @Body() body : any){
-      try{
-        const user = JSON.parse(JSON.stringify(body));
-        let result = await this.fileconvertService.uploadFile(request, response, files, user);
-=======
+
   async uploadFile(@Req() request, @Res() response, @UploadedFiles() files : Express.Multer.File[], @Body(new ValidationPipe()) body : CreateFilesDto){
       try{
         const user : CreateFilesDto = JSON.parse(JSON.stringify(body));
         let result : Express.Multer.File[] = await this.fileconvertService.uploadFile(request, response, files, user);
->>>>>>> 323b0f0b5748f96e8cb104951a116e243c2db3c2
         return response.json(result);
       } catch(err){
         console.error(err);
@@ -35,7 +26,6 @@ export class FileconvertController {
   }
 
   @Post('/convertFile')
-<<<<<<< HEAD
   async convertFile(@Req() request, @Res() response, @Body() body : any){
     try{
       const result = await this.fileconvertService.convertFile(request, response, body);
@@ -49,9 +39,6 @@ export class FileconvertController {
 
   @Post('/downloadFile')
   async downloadFile(@Req() request, @Res() response, @Body() body : any){
-    
-=======
-  async convertFile(@Req() request, @Res() response, @Body() body){
->>>>>>> 323b0f0b5748f96e8cb104951a116e243c2db3c2
+  
   }
 }
--- compressiony_back/src/repository/originalFile.repository.ts
+++ compressiony_back/src/repository/originalFile.repository.ts
@@ -7,9 +7,4 @@ export class OriginalFileRepository extends Repository<OriginalFile> {
     constructor(private dataSource : DataSource){
         super(OriginalFile, dataSource.createEntityManager());
     }
-<<<<<<< HEAD
-
-    
-=======
->>>>>>> 323b0f0b5748f96e8cb104951a116e243c2db3c2
 }
